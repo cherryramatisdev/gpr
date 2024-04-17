6a65607f828ebd5163e1bdec8e3d6e2850ee645c
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-04-16 22:23:37 -0300

    Add initial support for rendering lists

--- main.go
+++ main.go
@@ -4,13 +4,56 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{}
+const listHeight = 14
+
+type pr struct {
+	title  string
+	desc   string
+	org    string
+	repo   string
+	number int
+}
+
+func (p pr) Title() string       { return p.title }
+func (p pr) Description() string { return p.desc }
+func (p pr) FilterValue() string { return p.title }
+
+type model struct {
+	prs list.Model
+}
 
 func initialModel() model {
-	return model{}
+	items := []list.Item{
+		pr{
+			title:  "PR TITLE 1",
+			org:    "cherryramatisdev",
+			repo:   "gpr",
+			number: 700,
+		},
+		pr{
+			title:  "PR TITLE 2",
+			org:    "cherryramatisdev",
+			repo:   "gpr",
+			number: 700,
+		},
+		pr{
+			title:  "PR TITLE 3",
+			org:    "cherryramatisdev",
+			repo:   "gpr",
+			number: 700,
+		},
+	}
+
+	const defaultWidth = 20
+
+	prs := list.New(items, list.NewDefaultDelegate(), defaultWidth, listHeight)
+	prs.Title = "Title"
+
+	return model{prs: prs}
 }
 
 func (m model) Init() tea.Cmd {
@@ -26,17 +69,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.prs, cmd = m.prs.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	s := "Hello World"
-
-	return s
+	return m.prs.View()
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Deu merda menor %v", err)
